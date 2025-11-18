5fd0b74733cdbae009e3f20f95137361aac3b784
Sh0ookshed <185430047+Sh0ookshed@users.noreply.github.com>
2025-11-18 11:52:40 +0000

    completed setup for bishop class (need image)

--- chess_pieces/bishop.py
+++ chess_pieces/bishop.py
@@ -1,11 +1,35 @@
-#file containing the child class for the bishop chess piece
-
-#libraries
-import pygame
-
-#file imports
-from _chess_piece import chess_piece
+#------------------------------------------------------------------------------
+#BISHOP
+#------------------------------------------------------------------------------
 
 #bishop chess piece class containing all animations and validation + frontend for bishop
+
+#------------------------------------------------------------------------------
+#libraries
+#------------------------------------------------------------------------------
+import pygame #GUI
+
+#------------------------------------------------------------------------------
+#initialisation
+#------------------------------------------------------------------------------
+pygame.init()
+
+#------------------------------------------------------------------------------
+#Chess piece class
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+#file imports
+#------------------------------------------------------------------------------
+from _chess_piece import chess_piece
+
+#------------------------------------------------------------------------------
+#bishop class
+#------------------------------------------------------------------------------
 class bishop_p(chess_piece):
-    pass
+    def __init__(self, colour, p_ID, square):
+        super().__init__(colour, p_ID, square)
+
+        #
+        self.image = pygame.image.load("assets/general pictures/placeholder.png").convert() #change image to bishop image once created
+        self.p_value = 3 #how many points the bishop is worth
